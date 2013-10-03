d2df5883a2f1dbd57ee9ba9c0f94e01e9c96deb0
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2013-10-03 20:20:37 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,61 @@
+class lustre_client ( $version="2.1") {
+  if ($version=="2.1") {
+
+
+    file { '/etc/yum.repos.d/oxford-local-lustre-2.1-client.repo':
+    ensure  => present,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/lustre_client/oxford-local-lustre-2.1-client.repo',
+
+    }
+  }
+  ensure_packages ( [ "lustre-client", "lustre-client-modules" ] )
+#:  #libcomm_err
+#                ensure =>installed,
+#                require => File['/etc/yum.repos.d/oxford-local-lustre-2.1-client.repo']
+#          }
+
+  mount { "/lustre/lhcb":
+    device  => "pplxlustremds:/lhcb",
+    fstype  => "lustre",
+    ensure  => "mounted",
+    options => "localflock",
+    atboot  => "true",
+    require => File['/lustre/lhcb'],
+    }
+  mount { "/lustre/atlas":
+    device  => "pplxlustremds:/atlas",
+    fstype  => "lustre",
+    ensure  => "mounted",
+    options => "localflock",
+    atboot  => "true",
+    require => File['/lustre/atlas'],
+    }
+
+  file { '/lustre' :
+  ensure  => 'directory',
+  mode    => '0744',
+  owner   => 'root',
+  group   => 'root',
+  }
+  file { '/lustre/lhcb' :
+  ensure  => 'directory',
+  mode    => '0744',
+  replace => "no",
+  owner   => 'root',
+  group   => 'root',
+  require => File['/lustre'],
+  }
+  file { '/lustre/atlas' :
+  ensure  => 'directory',
+  mode    => '0744',
+  replace => "no",
+  owner   => 'root',
+  group   => 'root',
+  require => File['/lustre'],
+  }
+
+
+}
